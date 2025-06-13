4181dc4d6fdbd3442801c8d1955b503561fb7b73
munjungmin <answjdals77@gmail.com>
2025-06-14 02:33:12 +0900

    feat: Approval DTO 정의

---
+++ oliveyoung/src/main/java/com/olive/common/model/Approval.java
@@ -0,0 +1,36 @@
+package com.olive.common.model;
+
+import java.sql.Date;
+
+public class Approval {
+	
+	private int approval_id;
+	//private Ib ib;
+	private User user;
+	private Date approval_date;
+	
+	public int getApproval_id() {
+		return approval_id;
+	}
+	public void setApproval_id(int approval_id) {
+		this.approval_id = approval_id;
+	}
+//	public Ib getIb() {
+//		return ib;
+//	}
+//	public void setIb(Ib ib) {
+//		this.ib = ib;
+//	}
+	public User getUser() {
+		return user;
+	} 
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public Date getApproval_date() {
+		return approval_date;
+	}
+	public void setApproval_date(Date approval_date) {
+		this.approval_date = approval_date;
+	}
+}
