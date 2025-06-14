d2d0e4f3a7fcf0f3de7638e8f801484a7e9f74b7
munjungmin <answjdals77@gmail.com>
2025-06-14 18:36:36 +0900

    Feat: 입출고 관련 DTO 정의

--- oliveyoung/src/main/java/com/olive/common/model/Approval.java
+++ oliveyoung/src/main/java/com/olive/common/model/Approval.java
@@ -5,7 +5,7 @@ import java.sql.Date;
 public class Approval {
 	
 	private int approval_id;
-	//private Ib ib;
+	private Inbound inbound;
 	private User user;
 	private Date approval_date;
 	
@@ -15,15 +15,16 @@ public class Approval {
 	public void setApproval_id(int approval_id) {
 		this.approval_id = approval_id;
 	}
-//	public Ib getIb() {
-//		return ib;
-//	}
-//	public void setIb(Ib ib) {
-//		this.ib = ib;
-//	}
+	
+	public Inbound getInbound() {
+		return inbound;
+	}
+	public void setInbound(Inbound inbound) {
+		this.inbound = inbound;
+	}
 	public User getUser() {
 		return user;
-	} 
+	}
 	public void setUser(User user) {
 		this.user = user;
 	}
