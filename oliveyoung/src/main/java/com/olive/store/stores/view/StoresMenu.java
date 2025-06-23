4beac16fe0674fe7218001a3e3aba257cc7c7efa
owhitekitty <owhitekitty@gmail.com>
2025-06-23 17:43:13 +0900

    Feat: Add DB-Connected Chart, OutBoundDAO Fix: ComboBoxUtil, Branch Query

--- oliveyoung/src/main/java/com/olive/store/stores/view/StoresMenu.java
+++ oliveyoung/src/main/java/com/olive/store/stores/view/StoresMenu.java
@@ -51,7 +51,7 @@ public class StoresMenu extends Panel {
 		scroll = new JScrollPane(table);
 
 		branchDAO = new BranchDAO();
-		list = branchDAO.selectBranchDetail(storeName);
+		list = branchDAO.selectBranchDetail(storeName);	// 지점의 상세 정보를 가져옴
 		lb_top = new JLabel("담당자 : " + list.get(0).getUser().getUser_name() + "     이메일 : " + list.get(0).getUser().getEmail() + "     연락처 : " + list.get(0).getUser().getTel());
 		lb_bottom = new JLabel("매장 주소 : " + list.get(0).getBr_address() + "     매장 전화 : " + list.get(0).getBr_tel());
 
@@ -68,7 +68,7 @@ public class StoresMenu extends Panel {
 		lb_title.setFont(new Font("Noto Sans KR", Font.BOLD, 26));
 		lb_title.setHorizontalAlignment(JLabel.RIGHT);
 
-		p_details.setPreferredSize(new Dimension(Config.CONTENT_W, 100));
+		p_details.setPreferredSize(new Dimension(Config.CONTENT_W, 80));
 		p_details.setBorder(BorderFactory.createEmptyBorder(0, 500, 0, 50));
 		p_details.setLayout(new FlowLayout(FlowLayout.RIGHT));
 		p_details.setOpaque(false);
