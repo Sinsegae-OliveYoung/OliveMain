dcbe78cb01b3153b6acd25744123f25c32893bb9
munjungmin <answjdals77@gmail.com>
2025-06-25 10:10:29 +0900

    chat

---
+++ oliveyoung/src/main/java/com/olive/chat/Client.java
@@ -0,0 +1,18 @@
+package com.olive.chat;
+
+public class Client {
+	
+	
+	public Client() {
+		
+	}
+	
+	public static void main(String[] args) {
+		new Client();
+	}
+
+}
+
+/**
+ * 클라이언트는 접속을 해야됨 
+ */
