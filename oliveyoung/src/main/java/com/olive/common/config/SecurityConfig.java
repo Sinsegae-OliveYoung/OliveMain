69c9ec1779bae926730d35ce1de01a34999705cb
min023dev <lovekmj23@gmail.com>
2025-06-30 00:52:48 +0900

    Revert "Delete oliveyoung/src/main/java/com/olive/common/config directory"
    This reverts commit 0e58a928f635fc95121b35f8273aa72cdc91f5f2.

---
+++ oliveyoung/src/main/java/com/olive/common/config/SecurityConfig.java
@@ -0,0 +1,8 @@
+package com.olive.common.config;
+
+public class SecurityConfig {
+
+    public static final String account_user = "o1357dd@gmail.com";
+    public static final String app_pwd = "pdhi anva dzix kzbt";
+    public static final String IP = "192.168.10.101";
+}
