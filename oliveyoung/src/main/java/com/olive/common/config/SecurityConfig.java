2c765dff441b37a7e300e2ec98557d28316e0f42
Minji Kim <lovekmj23@gmail.com>
2025-06-30 00:41:45 +0900

    Delete oliveyoung/src/main/java/com/olive/common/config/SecurityConfig.java
    securityConfig delete

--- oliveyoung/src/main/java/com/olive/common/config/SecurityConfig.java
+++
@@ -1,8 +0,0 @@
-package com.olive.common.config;
-
-public class SecurityConfig {
-
-    public static final String account_user = "o1357dd@gmail.com";
-    public static final String app_pwd = "pdhi anva dzix kzbt";
-    public static final String IP = "192.168.10.101";
-}
